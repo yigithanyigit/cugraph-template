6cebd5be48ff2c0f0af3de65fa3a83bd7ed3e93a
Yigithan Yigit <yigithanyigitdevel@gmail.com>
2024-12-13 03:02:09 +0300

    New runner script for

---
+++ runner-louvain.py
@@ -0,0 +1,71 @@
+#!usr/bin/python
+
+# This scripts running program with the given input files
+#
+# Usage: ./runner.py <executable path> <args of executable> <input_file dir>
+
+import os
+import sys
+import subprocess
+import argparse
+
+max_level =  [50,50,50]
+threshold =  [1e-3,1e-5,1e-7]
+resolution = [1.0, 1.0, 1.0]
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Run the program with the given input files')
+    parser.add_argument('executable', help='The path of the executable')
+    #parser.add_argument('args', nargs=argparse.REMAINDER, help='The arguments of the executable')
+    parser.add_argument('input_dir', help='The directory of the input files')
+    parser.add_argument('output_dir', help='The directory of the input files')
+    args = parser.parse_args()
+
+    if not os.path.isdir(args.input_dir):
+        print("The input directory is not exist")
+        sys.exit(1)
+
+    if not os.path.isdir(args.output_dir):
+        print("The output directory is not exist")
+        print("Creating the output directory")
+        os.mkdir(args.output_dir)
+        print("The output directory is created")
+
+    input_files = os.listdir(args.input_dir)
+
+    for input_file in input_files:
+        input_path = os.path.join(args.input_dir, input_file)
+        print("Running the program with input file: %s" % input_path)
+        #subprocess.run([args.executable] + args.args + [input_path], capture_output=True, text=True)
+        for level, thres, res in zip(max_level, threshold, resolution):
+
+            print(f"{args.executable} {level} {thres} {res} {input_path}")
+            result = subprocess.run([args.executable] + [input_path] + [str(level), str(thres), str(res)], capture_output=True, text=True)
+            
+            if result.returncode != 0:
+                print("Error: %s" % result.stderr)
+                sys.exit(1)
+
+            output_file  = os.path.join(args.output_dir, f"{input_file}_{level}_{thres}_{res}.out")
+            with open(output_file, "w") as f:
+                f.write(result.stdout)
+
+
+            nsys_command = ["nsys", "profile", "--output", f"{args.output_dir}/{input_file}_{level}_{thres}_{res}_nsys_report", args.executable] + [input_path] + [str(level), str(thres), str(res)]
+            
+            print(f"Running command: {' '.join(nsys_command)}")
+            result = subprocess.run(nsys_command, capture_output=True, text=True)
+            
+            if result.returncode != 0:
+                print("Error: %s" % result.stderr)
+                sys.exit(1)
+            
+            # Save stdout to a file
+            output_file = os.path.join(args.output_dir, f"{input_file}_{level}_{thres}_{res}_nsys.out")
+            with open(output_file, 'w') as f:
+                f.write(result.stdout)
+
+
+if __name__ == '__main__':
+    main()
